6580981a35d2ea4bf093f65f41f7622d9afd5e5d
zaher-m <maredazaher@gmail.com>
2025-11-01 16:24:46 +0200

    update function names

--- src/edu/iti/Maven.groovy
+++ src/edu/iti/Maven.groovy
@@ -1,11 +1,11 @@
 package edu.iti
 
-def mavenBuild() {
+def build() {
     echo "Running Maven Build..."
     sh "/usr/bin/mvn clean install"
 }
 
-def mavenTest() {
+def test() {
     echo "Running Maven Tests..."
     sh "/usr/bin/mvn test"
 }
